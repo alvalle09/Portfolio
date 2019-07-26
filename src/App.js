e1269e1750e6464502f65f325da3d7c69ff44406
alvalle09 <phredvalle@gmail.com>
2019-07-25 23:29:42 -0500

    Add App component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'; 
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>I'm AlFredo Valle</p>
+                <p>Looking forward to work on new and exciting projects!</p>
+            </div>
+        )
+    }
+}
+
+export default App;
