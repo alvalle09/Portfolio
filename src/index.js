913e16daa67ef08cfc02dbdfb982272127a2c0b9
alvalle09 <phredvalle@gmail.com>
2019-07-25 23:50:58 -0500

    extend classes practice

--- src/index.js
+++ src/index.js
@@ -2,5 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<div>React Element</div>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
+class Animal {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    speak() {
+        console.log('I am', this.name, 'and I am', this.age, 'years old');
+    }
+}
+
+const animal1 = new Animal('Simba', 33);
+animal1.speak();
+
+console.log(animal1);
+
+class Lion extends Animal {
+    constructor(name, age, furColor, speed) {
+        //bind name and age to Animal class constructor
+        super(name, age);
+        this.furColor = furColor;
+        this.speed = speed;
+    }
+}
