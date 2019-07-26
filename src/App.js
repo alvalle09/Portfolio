e44f8773ef186bd41c2e5c94d5b1405f58b36f4f
Fred Valle <phredvalle@gmail.com>
2019-07-26 16:44:28 -0500

    adding dynamic bio view using state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react'; 
 
-class RegularClass {};
-class ComponentClass extends Component {};
+// class RegularClass {};
+// class ComponentClass extends Component {};
 
-const regClassInstance = new RegularClass();
-const componentClassInstance = new ComponentClass();
+// const regClassInstance = new RegularClass();
+// const componentClassInstance = new ComponentClass();
 
-console.log('regClassInstance', regClassInstance);
-console.log('ComponentClassInstance', componentClassInstance);
+// console.log('regClassInstance', regClassInstance);
+// console.log('ComponentClassInstance', componentClassInstance);
+
 
 class App extends Component {
+    constructor() {
+        super(); // need this line otherwise "this" is not available
+        this.state = {
+            displayBio: false 
+        };
+    }
+
     render() {
+        let bio;
+
         return (
             <div>
                 <h1>Hello!</h1>
                 <p>I'm AlFredo Valle</p>
                 <p>Looking forward to work on new and exciting projects!</p>
+                <div>
+                    <p>I live in the Rio Grande Valley in beautiful South Texas, and am passionate about coding. </p>
+                    <p>My favorit language is Javascript, and I enjoy React and Angular as well.</p>
+                    <p>When I am not coding, I spend time in the outdoors fishing or camping, or playing drums!</p>
+                </div>
             </div>
         )
     }
