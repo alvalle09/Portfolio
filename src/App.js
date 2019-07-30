9b90c45ba92448ace2f344b1d56e2459b62cee1d
alvalle09 <phredvalle@gmail.com>
2019-07-29 22:39:09 -0500

    adding projects list component

--- src/App.js
+++ src/App.js
@@ -1,52 +1,46 @@
-import React, { Component } from 'react'; 
-
-// class RegularClass {};
-// class ComponentClass extends Component {};
-
-// const regClassInstance = new RegularClass();
-// const componentClassInstance = new ComponentClass();
-
-// console.log('regClassInstance', regClassInstance);
-// console.log('ComponentClassInstance', componentClassInstance);
+import React, { Component } from "react";
+import Projects from './Projects';
 
 
 class App extends Component {
-    state = { displayBio: false} ;
+  state = { displayBio: false };
 
-    toggleDisplayBio = () => {
-        this.setState( {displayBio: !this.state.displayBio });
-    }
+  toggleDisplayBio = () => {
+    this.setState({ displayBio: !this.state.displayBio });
+  };
 
-    render() {
-        return (
-            <div>
-                <h1>Hello!</h1>
-                <p>I'm AlFredo Valle</p>
-                <p>Looking forward to work on new and exciting projects!</p>
-                {this.state.displayBio ? (
-                    <div>
-                      <p>
-                        I live in San Benito in the Rio Grande Valley area of beautiful south Texas,
-                        and am passionate about coding.
-                      </p>
-                      <p>
-                        My favorit language is Javascript, and I enjoy React and
-                        Angular as well.
-                      </p>
-                      <p>
-                        When I am not coding, I like to spend time in the outdoors fishing
-                        and camping, or playing drums!
-                      </p>
-                      <button onClick={this.toggleDisplayBio}>Show less</button>
-                    </div>
-                  ) : (
-                      <div>
-                        <button onClick={this.toggleDisplayBio}>Read more</button>
-                      </div>
-                  ) }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+        <p>My name is AlFredo Valle. I'm a software developer.</p>
+        <p>I'm looking forward to work on new and exciting projects!</p>
+        {this.state.displayBio ? (
+          <div>
+            <p>
+              I live in San Benito in the Rio Grande Valley area of beautiful
+              south Texas, and am passionate about coding.
+            </p>
+            <p>
+              My favorit language is Javascript, and I enjoy React and Angular
+              as well.
+            </p>
+            <p>
+              When I am not coding, I like to spend time in the outdoors fishing
+              and camping, or playing drums!
+            </p>
+            <button onClick={this.toggleDisplayBio}>Show less</button>
+          </div>
+        ) : (
+          <div>
+            <button onClick={this.toggleDisplayBio}>Read more</button>
+          </div>
+        )}
+        <hr />
+        <Projects />
+      </div>
+    );
+  }
 }
 
 export default App;
