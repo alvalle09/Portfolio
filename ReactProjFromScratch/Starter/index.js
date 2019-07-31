0402a8617891ade9158bdce4bb3ad46e343bcdf1
alvalle09 <phredvalle@gmail.com>
2019-07-30 22:15:23 -0500

    use parcel bundler instead of react cdn

--- ReactProjFromScratch/Starter/index.js
+++ ReactProjFromScratch/Starter/index.js
@@ -1,4 +1,5 @@
 
-const element = React.createElement('div', null, 'My React Element!');
+//const element = React.createElement('div', null, 'My React Element!');
+const element = <div>REact Element with JSX</div>;
 
 ReactDOM.render(element, document.getElementById('root'));
