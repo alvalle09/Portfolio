a64635a66e4ad86c05daa37e4bc968f656fb8bfb
alvalle09 <phredvalle@gmail.com>
2019-08-19 21:12:38 -0500

    fetch 10 jokes at a time

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
 
 class Jokes extends Component {
-    state = { joke: {} };
+    state = { joke: {}, jokes: []  }; // add jokes array for fetching 10 jokes
 
     componentDidMount() {
         fetch('https://official-joke-api.appspot.com/random_joke')
         .then(response => response.json())
-        // .then(json => console.log('json', json));
+        //.then(json => console.log('json', json))
         .then(json => this.setState({ joke: json}));
 
     }
 
-    render() {
-        
+    fetchJokes = () => {
+        fetch('https://official-joke-api.appspot.com/random_ten')
+        .then(response => response.json())
+        .then(json => this.setState({ jokes: json}));
+
+    }
+
+    render() {        
         const { setup, punchline } = this.state.joke;
 
         return (
             <div>
                 <h2>Highlighted Joke</h2>
                 <p>{setup} <em>{punchline}</em></p>
+                <hr />
+                <h3>10 more jokes???</h3>
+                <button onClick={this.fetchJokes} >Click me!</button>
+                {
+                    this.state.jokes.map(joke => {
+                        const { id, setup, punchline } = joke;
+
+                        return <p key={id}>{setup} <em>{punchline} </em></p>
+                    })
+                }
             </div>
         )
     }
