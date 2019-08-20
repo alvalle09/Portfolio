70074efed2ce747a2bb4670ab9ab5b7e59c65d4a
alvalle09 <phredvalle@gmail.com>
2019-08-19 23:20:49 -0500

    resolve promise

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import './index.css';
+import { resolve } from "path";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
+new Promise(resolve => {
+    setTimeout(() => {
+         console.log('Bears');
+         resolve();  //proimse has completed, keyword callback
+    }, 2000);
+})
+.then(() => {
+    console.log('Beets');
+    console.log('Carrots');
+});
 
 
