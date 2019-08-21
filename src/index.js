5be5f8e507c33abb70e6cdf098d095b01799da47
alvalle09 <phredvalle@gmail.com>
2019-08-20 19:23:09 -0500

    add error handling for fetch

--- src/index.js
+++ src/index.js
@@ -6,17 +6,17 @@ import { resolve } from "path";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-new Promise((resolve, reject) => {
-    // use return keyword to ensure we escape the function for testing catch
-    return reject(new Error('No Bears'));
-    setTimeout(() => {
-         resolve('Bears, and lions, oh my');  //proimse has completed, keyword callback
-    }, 2000);
-})
-.then( quote => {
-    console.log(quote);
-    
-})
-.catch(error => console.log('error', error));
+// Experimental code for testing promises
+// new Promise((resolve, reject) => {
+//     // use return keyword to ensure we escape the function for testing catch
+//     return reject(new Error('No Bears'));
+//     setTimeout(() => {
+//          resolve('Bears, and lions, oh my');  //proimse has completed, keyword callback
+//     }, 2000);
+// })
+// .then( quote => {
+//     console.log(quote);    
+// })
+// .catch(error => console.log('error', error));
 
 
