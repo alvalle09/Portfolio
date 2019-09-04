d76745e906f4d970f2c1c8cbd9cdd0cf8409500d
alvalle09 <phredvalle@gmail.com>
2019-09-04 01:04:02 -0500

    incorporate music-master into profile

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { Router, Switch, Route } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import App from './components/App';
 import Header from './components/Header';
-
 import Jokes from './components/Jokes';
+import MusicMaster from './projects/music-master';
 import './index.css';
 import { resolve } from "path";
 
@@ -17,6 +17,7 @@ ReactDOM.render(
         <Switch>
             <Route exact path='/' render={() => <Header> <App /> </Header> } />
             <Route path='/jokes' render={() => <Header> <Jokes /> </Header> } />
+            <Route path='/music-master' render={() => <Header> <MusicMaster/> </Header> } />
         </Switch>
     </Router>,
     document.getElementById("root")
