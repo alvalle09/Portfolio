d5f27f58af6b97c5483cd42ade2ecb3cd571a16c
alvalle09 <phredvalle@gmail.com>
2019-10-28 20:12:51 -0500

    remove starter files project

--- ReactProjFromScratch/Starter/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-//const element = React.createElement('div', null, 'My React Element!');
-const element = <div>React Element with JSX</div>;
-
-ReactDOM.render(element, document.getElementById('root'));
--- src/projects/music-master/components/App.js
+++ src/projects/music-master/components/App.js
@@ -13,7 +13,6 @@ class App extends Component {
 
   componentDidMount() {
     this.searchArtist('The Beatles');
-    
   }
 
 
